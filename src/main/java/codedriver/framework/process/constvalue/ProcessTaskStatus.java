56f6be21cac12e9723f58e3cde2599f45771d362
吕佐康 <lvzk@techsure.com.cn>
2020-04-03 11:12:25 +0800

    update processtask status color

--- src/main/java/codedriver/framework/process/constvalue/ProcessTaskStatus.java
+++ src/main/java/codedriver/framework/process/constvalue/ProcessTaskStatus.java
@@ -6,23 +6,25 @@ import java.util.List;
 import codedriver.framework.common.dto.ValueTextVo;
 
 public enum ProcessTaskStatus {
-	RUNNING("running", "处理中"),
-	ABORTED("aborted", "已终止"),
-	SUCCEED("succeed", "已成功"),
-	PENDING("pending", "待处理"),
-	FAILED("failed", "已失败"),
-	ABORTING("aborting", "终止中"),
-	BACK("back", "已回退"),
-	HANG("hang", "已挂起"),
-	DRAFT("draft", "我的草稿");
+	RUNNING("running", "处理中","#2d84fb"),
+	ABORTED("aborted", "已终止","#2d84fb"),
+	SUCCEED("succeed", "已成功","#2d84fb"),
+	PENDING("pending", "待处理","#2d84fb"),
+	FAILED("failed", "已失败","#2d84fb"),
+	ABORTING("aborting", "终止中","#2d84fb"),
+	BACK("back", "已回退","#2d84fb"),
+	HANG("hang", "已挂起","#2d84fb"),
+	DRAFT("draft", "未提交","#2d84fb");
 	
 	private String status;
 	private String text;
+	private String color;
 
 	private static List<ValueTextVo> processTaskStatusList;
-	private ProcessTaskStatus(String _status, String _text) {
+	private ProcessTaskStatus(String _status, String _text,String _color) {
 		this.status = _status;
 		this.text = _text;
+		this.color = _color;
 	}
 
 	public String getValue() {
@@ -31,6 +33,10 @@ public enum ProcessTaskStatus {
 
 	public String getText() {
 		return text;
+	}
+
+	public String getColor() {
+		return color;
 	}
 
 	public static String getValue(String _status) {
@@ -46,6 +52,15 @@ public enum ProcessTaskStatus {
 		for (ProcessTaskStatus s : ProcessTaskStatus.values()) {
 			if (s.getValue().equals(_status)) {
 				return s.getText();
+			}
+		}
+		return "";
+	}
+	
+	public static String getColor(String _status) {
+		for (ProcessTaskStatus s : ProcessTaskStatus.values()) {
+			if (s.getValue().equals(_status)) {
+				return s.getColor();
 			}
 		}
 		return "";
