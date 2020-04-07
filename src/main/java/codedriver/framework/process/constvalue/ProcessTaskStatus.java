c8527bb511db2c82428bb09d89a27ea5975daa91
吕佐康 <lvzk@techsure.com.cn>
2020-04-07 18:50:18 +0800

    update processstatus color

--- src/main/java/codedriver/framework/process/constvalue/ProcessTaskStatus.java
+++ src/main/java/codedriver/framework/process/constvalue/ProcessTaskStatus.java
@@ -7,14 +7,14 @@ import codedriver.framework.common.dto.ValueTextVo;
 
 public enum ProcessTaskStatus {
 	RUNNING("running", "处理中","#2d84fb"),
-	ABORTED("aborted", "已终止","#f71010"),
+	ABORTED("aborted", "已终止","#8E949F"),
 	SUCCEED("succeed", "已成功","#25b865"),
-	PENDING("pending", "待处理","#f71010"),
+	PENDING("pending", "待处理","#8E949F"),
 	FAILED("failed", "已失败","#f71010"),
-	ABORTING("aborting", "终止中","#f71010"),
-	BACK("back", "已回退","#f71010"),
-	HANG("hang", "已挂起","#f71010"),
-	DRAFT("draft", "未提交","#f71010");
+	ABORTING("aborting", "终止中","#8E949F"),
+	BACK("back", "已回退","#8E949F"),
+	HANG("hang", "已挂起","#8E949F"),
+	DRAFT("draft", "未提交","#8E949F");
 	
 	private String status;
 	private String text;
