3bcdb60fcc569dd571e3d4c0681bd992b127e53f
lixs <lixs@techsure.com.cn>
2020-04-10 19:01:36 +0800

    矩阵属性数据调整

---
+++ src/main/java/codedriver/framework/process/dto/ProcessMatrixColumnVo.java
@@ -0,0 +1,27 @@
+package codedriver.framework.process.dto;
+
+/**
+ * @program: codedriver
+ * @description:
+ * @create: 2020-04-10 18:25
+ **/
+public class ProcessMatrixColumnVo {
+    private String column;
+    private String value;
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
