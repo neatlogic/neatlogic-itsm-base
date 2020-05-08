95acbedf719a0be3b0bcc88010ac5c78da61d77f
林邦泉 <linbq@techsure.com.cn>
2020-05-08 18:34:36 +0800

    --story=1002853 --user=林邦泉 矩阵接口联调 https://www.tapd.cn/54247054/s/1063219

--- src/main/java/codedriver/framework/process/dto/ProcessMatrixExternalVo.java
+++ src/main/java/codedriver/framework/process/dto/ProcessMatrixExternalVo.java
@@ -6,17 +6,8 @@ package codedriver.framework.process.dto;
  * @create: 2020-04-02 18:25
  **/
 public class ProcessMatrixExternalVo {
-//    private String uuid;
     private String matrixUuid;
     private String integrationUuid;
-
-//    public String getUuid() {
-//        return uuid;
-//    }
-//
-//    public void setUuid(String uuid) {
-//        this.uuid = uuid;
-//    }
 
     public String getMatrixUuid() {
         return matrixUuid;
