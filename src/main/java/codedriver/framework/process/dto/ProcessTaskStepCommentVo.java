79f090c047025ee967909cd9ab565bdda39a2bbb
林邦泉 <linbq@techsure.com.cn>
2020-05-15 12:08:37 +0800

    --story=1002161 --user=林邦泉 查看页面相关接口 https://www.tapd.cn/54247054/s/1064968

--- src/main/java/codedriver/framework/process/dto/ProcessTaskStepCommentVo.java
+++ src/main/java/codedriver/framework/process/dto/ProcessTaskStepCommentVo.java
@@ -1,6 +1,7 @@
 package codedriver.framework.process.dto;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.alibaba.fastjson.JSON;
@@ -13,16 +14,32 @@ import codedriver.framework.process.constvalue.ProcessTaskAuditDetailType;
 import codedriver.framework.restful.annotation.EntityField;
 
 public class ProcessTaskStepCommentVo {
-	//private Long auditId;
+	@EntityField(name = "回复id", type = ApiParamType.LONG)
+	private Long id;
 	@EntityField(name = "描述内容", type = ApiParamType.STRING)
 	private String content;
 	@EntityField(name = "附件列表", type = ApiParamType.JSONARRAY)
 	private List<FileVo> fileList = new ArrayList<>();
+	@EntityField(name = "创建用户", type = ApiParamType.STRING)
+	private String fcu;
+	@EntityField(name = "创建用户名称", type = ApiParamType.STRING)
+	private String fcuName;
+	@EntityField(name = "最后一次修改用户", type = ApiParamType.STRING)
+	private String lcu;
+	@EntityField(name = "最后一次修改用户名称", type = ApiParamType.STRING)
+	private String lcuName;
+	@EntityField(name = "创建时间", type = ApiParamType.LONG)
+	private Date fcd;
+	@EntityField(name = "最后一次修改时间", type = ApiParamType.LONG)
+	private Date lcd;
 	
 	public ProcessTaskStepCommentVo() {
 	}
 	public ProcessTaskStepCommentVo(ProcessTaskStepAuditVo processTaskStepAuditVo) {
-		//auditId = processTaskStepAuditVo.getId();
+		this.id = processTaskStepAuditVo.getId();
+		this.fcd = processTaskStepAuditVo.getActionTime();
+		this.fcu = processTaskStepAuditVo.getUserId();
+		this.fcuName = processTaskStepAuditVo.getUserName();
 		List<ProcessTaskStepAuditDetailVo> processTaskStepAuditDetailList = processTaskStepAuditVo.getAuditDetailList();
 		for(ProcessTaskStepAuditDetailVo processTaskStepAuditDetailVo : processTaskStepAuditDetailList) {
 			IProcessTaskStepAuditDetailHandler auditDetailHandler = ProcessTaskStepAuditDetailHandlerFactory.getHandler(processTaskStepAuditDetailVo.getType());		
@@ -30,11 +47,11 @@ public class ProcessTaskStepCommentVo {
 				if(auditDetailHandler != null) {
 					auditDetailHandler.handle(processTaskStepAuditDetailVo);
 				}
-				content = processTaskStepAuditDetailVo.getNewContent();
+				this.content = processTaskStepAuditDetailVo.getNewContent();
 			}else if(ProcessTaskAuditDetailType.FILE.getValue().equals(processTaskStepAuditDetailVo.getType())){
 				if(auditDetailHandler != null) {
 					auditDetailHandler.handle(processTaskStepAuditDetailVo);
-					fileList = JSON.parseArray(processTaskStepAuditDetailVo.getNewContent(), FileVo.class);
+					this.fileList = JSON.parseArray(processTaskStepAuditDetailVo.getNewContent(), FileVo.class);
 				}
 			}
 		}
@@ -51,12 +68,47 @@ public class ProcessTaskStepCommentVo {
 	public void setFileList(List<FileVo> fileList) {
 		this.fileList = fileList;
 	}
-
-//	public Long getAuditId() {
-//		return auditId;
-//	}
-//	public void setAuditId(Long auditId) {
-//		this.auditId = auditId;
-//	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFcu() {
+		return fcu;
+	}
+	public void setFcu(String fcu) {
+		this.fcu = fcu;
+	}
+	public String getFcuName() {
+		return fcuName;
+	}
+	public void setFcuName(String fcuName) {
+		this.fcuName = fcuName;
+	}
+	public String getLcu() {
+		return lcu;
+	}
+	public void setLcu(String lcu) {
+		this.lcu = lcu;
+	}
+	public String getLcuName() {
+		return lcuName;
+	}
+	public void setLcuName(String lcuName) {
+		this.lcuName = lcuName;
+	}
+	public Date getFcd() {
+		return fcd;
+	}
+	public void setFcd(Date fcd) {
+		this.fcd = fcd;
+	}
+	public Date getLcd() {
+		return lcd;
+	}
+	public void setLcd(Date lcd) {
+		this.lcd = lcd;
+	}
 
 }
