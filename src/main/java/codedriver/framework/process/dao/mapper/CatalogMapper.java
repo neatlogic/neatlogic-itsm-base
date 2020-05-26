cf5447889d69382bb00ed9902869831a1f4ddbe2
林邦泉 <linbq@techsure.com.cn>
2020-05-26 16:32:18 +0800

    --story=1002988 --user=林邦泉 服务目录增加左右编码逻辑 https://www.tapd.cn/54247054/s/1066500

--- src/main/java/codedriver/framework/process/dao/mapper/CatalogMapper.java
+++ src/main/java/codedriver/framework/process/dao/mapper/CatalogMapper.java
@@ -9,51 +9,51 @@ import codedriver.framework.process.dto.CatalogVo;
 
 public interface CatalogMapper {
 
-	List<CatalogVo> getCatalogList(CatalogVo catalogVo);
+	public List<CatalogVo> getCatalogList(CatalogVo catalogVo);
 
-	CatalogVo getCatalogByUuid(String uuid);
+	public CatalogVo getCatalogByUuid(String uuid);
 	
-	int checkCatalogIsExists(String catalogUuid);
+	public int checkCatalogIsExists(String catalogUuid);
 
-	List<String> getHasActiveChannelCatalogUuidList(List<String> channelUuidList);
+	public List<String> getHasActiveChannelCatalogUuidList(List<String> channelUuidList);
 
-	int checkCatalogNameIsRepeat(CatalogVo catalogVo);
+	public int checkCatalogNameIsRepeat(CatalogVo catalogVo);
 
-	List<CatalogVo> getCatalogListForTree(Integer isActive);
+	public List<CatalogVo> getCatalogListForTree(Integer isActive);
 
-	List<AuthorityVo> getCatalogAuthorityListByCatalogUuid(String uuid);
+	public List<AuthorityVo> getCatalogAuthorityListByCatalogUuid(String uuid);
 	
-	List<String> getAuthorizedCatalogUuidList(
+	public List<String> getAuthorizedCatalogUuidList(
 			@Param("userUuid")String userUuid, 
 			@Param("teamUuidList")List<String> teamUuidList, 
 			@Param("roleUuidList")List<String> roleUuidList, 
 			@Param("catalogUuid") String catalogUuid
 			);
 
-	String getCatalogLockByUuid(String uuid);
+	public String getCatalogLockByUuid(String uuid);
 
-	List<CatalogVo> getCatalogByParentUuid(String parentUuid);
+	public List<CatalogVo> getCatalogByParentUuid(String parentUuid);
 
-	int checkCatalogIsExistsByLeftRightCode(@Param("uuid")String uuid, @Param("lft") Integer lft, @Param("rht") Integer rht);
+	public int checkCatalogIsExistsByLeftRightCode(@Param("uuid")String uuid, @Param("lft") Integer lft, @Param("rht") Integer rht);
 
-	int getCatalogCount(CatalogVo catalogVo);
+	public int getCatalogCount(CatalogVo catalogVo);
 	
-	int replaceCatalog(CatalogVo catalogVo);
+	public int replaceCatalog(CatalogVo catalogVo);
 
-	int insertCatalogAuthority(@Param("authorityVo")AuthorityVo authorityVo,@Param("catalogUuid")String catalogUuid);
+	public int insertCatalogAuthority(@Param("authorityVo")AuthorityVo authorityVo,@Param("catalogUuid")String catalogUuid);
 
-	int updateCatalogParentUuidByUuid(CatalogVo catalogVo);
+	public int updateCatalogParentUuidByUuid(CatalogVo catalogVo);
 
-	int updateCatalogLeftRightCode(@Param("uuid") String uuid, @Param("lft") int lft, @Param("rht") int rht);
+	public int updateCatalogLeftRightCode(@Param("uuid") String uuid, @Param("lft") int lft, @Param("rht") int rht);
 
-	int batchUpdateCatalogLeftRightCodeByLeftRightCode(@Param("lft") Integer lft, @Param("rht") Integer rht, @Param("step") int step);
+	public int batchUpdateCatalogLeftRightCodeByLeftRightCode(@Param("lft") Integer lft, @Param("rht") Integer rht, @Param("step") int step);
 
-	int batchUpdateCatalogLeftCode(@Param("minCode") Integer minCode, @Param("step") int step);
+	public int batchUpdateCatalogLeftCode(@Param("minCode") Integer minCode, @Param("step") int step);
 
-	int batchUpdateCatalogRightCode(@Param("minCode") Integer minCode, @Param("step") int step);
+	public int batchUpdateCatalogRightCode(@Param("minCode") Integer minCode, @Param("step") int step);
 	
-	int deleteCatalogByUuid(String uuid);
+	public int deleteCatalogByUuid(String uuid);
 
-	int deleteCatalogAuthorityByCatalogUuid(String uuid);
+	public int deleteCatalogAuthorityByCatalogUuid(String uuid);
 
 }
