bb4a2dff0d33fdba8c0ae68156b43a995e4e877e
林邦泉 <linbq@techsure.com.cn>
2020-07-30 16:23:43 +0800

    --story=1002853 --user=林邦泉 矩阵接口联调 https://www.tapd.cn/54247054/s/1080146

--- src/main/java/codedriver/framework/process/formattribute/core/IFormAttributeHandler.java
+++ src/main/java/codedriver/framework/process/formattribute/core/IFormAttributeHandler.java
@@ -6,6 +6,9 @@ import codedriver.framework.process.dto.AttributeDataVo;
 import codedriver.framework.process.exception.form.AttributeValidException;
 
 public interface IFormAttributeHandler {
+	/** 下拉列表value和text列的组合连接符 **/
+	public final static String SELECT_COMPOSE_JOINER= "&=&";
+	
 	public String getType();
 
 	public boolean valid(AttributeDataVo attributeDataVo, JSONObject configObj) throws AttributeValidException;
