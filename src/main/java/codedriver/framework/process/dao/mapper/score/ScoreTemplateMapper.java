6f5b8d18b1630e82b025410e4000c646d011ff67
Aienao <laiwt@techsure.com.cn>
2020-08-27 15:25:23 +0800

    --story=1003247 --user=赖文韬 评分模版管理 https://www.tapd.cn/54247054/s/1084740

--- src/main/java/codedriver/framework/process/dao/mapper/score/ScoreTemplateMapper.java
+++ src/main/java/codedriver/framework/process/dao/mapper/score/ScoreTemplateMapper.java
@@ -1,14 +1,27 @@
 package codedriver.framework.process.dao.mapper.score;
 
+import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.process.dto.score.ScoreTemplateDimensionVo;
 import codedriver.framework.process.dto.score.ScoreTemplateVo;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface ScoreTemplateMapper {
+
+    public List<ScoreTemplateVo> searchScoreTemplate(ScoreTemplateVo scoreTemplateVo);
+
+    public int searchScoreTemplateCount(ScoreTemplateVo scoreTemplateVo);
+
+    public ScoreTemplateVo getScoreTemplateById(@Param("id") Long id);
 
     public int checkScoreTemplateNameIsRepeat(ScoreTemplateVo scoreTemplateVo);
 
     public ScoreTemplateVo checkScoreTemplateExistsById(@Param("id") Long id);
+
+    public List<ValueTextVo> getRefProcessList(ScoreTemplateVo scoreTemplateVo);
+
+    public int getRefProcessCount(@Param("scoreTemplateId") Long scoreTemplateId);
 
     public void updateScoreTemplate(ScoreTemplateVo scoreTemplateVo);
 
