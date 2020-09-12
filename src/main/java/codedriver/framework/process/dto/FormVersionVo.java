6db26631685d79d7cd32278b88cbe5f2c848b725
林邦泉 <linbq@techsure.com.cn>
2020-09-12 17:31:55 +0800

    --bug=1005606 --user=林邦泉 删除矩阵没有同步删除矩阵属性 https://www.tapd.cn/54247054/s/1088084

--- src/main/java/codedriver/framework/process/dto/FormVersionVo.java
+++ src/main/java/codedriver/framework/process/dto/FormVersionVo.java
@@ -16,7 +16,7 @@ import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.asynchronization.threadlocal.UserContext;
 import codedriver.framework.common.dto.BasePageVo;
-import codedriver.framework.process.dto.matrix.ProcessMatrixFormComponentVo;
+import codedriver.framework.matrix.dto.ProcessMatrixFormComponentVo;
 
 public class FormVersionVo extends BasePageVo implements Serializable {
 	private static final long serialVersionUID = 8345592242508980127L;
