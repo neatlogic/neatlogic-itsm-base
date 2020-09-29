720756df413bd7d25974ee9292c21c1449a6d41e
Aienao <laiwt@techsure.com.cn>
2020-09-29 16:56:46 +0800

    vip设置

--- src/main/java/codedriver/framework/process/dto/ProcessTaskVo.java
+++ src/main/java/codedriver/framework/process/dto/ProcessTaskVo.java
@@ -47,6 +47,8 @@ public class ProcessTaskVo {
     private String owner;
     @EntityField(name = "上报人", type = ApiParamType.STRING)
     private String ownerName;
+    @EntityField(name = "上报人等级", type = ApiParamType.STRING)
+    private String ownerVipLevel;
     @EntityField(name = "代报人userUuid", type = ApiParamType.STRING)
     private String reporter;
     @EntityField(name = "代报人", type = ApiParamType.STRING)
@@ -447,4 +449,11 @@ public class ProcessTaskVo {
         this.paramObj = paramObj;
     }
 
+    public String getOwnerVipLevel() {
+        return ownerVipLevel;
+    }
+
+    public void setOwnerVipLevel(String ownerVipLevel) {
+        this.ownerVipLevel = ownerVipLevel;
+    }
 }
