4979b2e2cd27cc9ed132fc03bafff6a9e92f57d5
林邦泉 <linbq@techsure.com.cn>
2020-11-12 12:03:51 +0800

    --story=1003619 --user=林邦泉 复合指标变化重新计算SLA https://www.tapd.cn/54247054/s/1104476

---
+++ src/main/java/codedriver/framework/process/constvalue/ConditionProcessTaskOptions.java
@@ -0,0 +1,31 @@
+package codedriver.framework.process.constvalue;
+
+public enum ConditionProcessTaskOptions {
+    TITLE("title", "标题"),
+    CHANNELTYPE("channeltype", "服务类型"),
+    CONTENT("content", "上报内容"),
+    STARTTIME("starttime", "开始时间"),
+    OWNER("owner", "上报人"),
+    PRIORITY("priority", "优先级"),
+    OWNERCOMPANY("ownercompany", "上报人公司");
+    private String value;
+    private String text;
+    private ConditionProcessTaskOptions(String value, String text) {
+        this.value = value;
+        this.text = text;
+    }
+    public String getValue() {
+        return value;
+    }
+    public String getText() {
+        return text;
+    }
+    public static ConditionProcessTaskOptions getConditionProcessTaskOprion(String _value) {
+        for(ConditionProcessTaskOptions e : values()) {
+            if(e.value.equals(_value)) {
+                return e;
+            }
+        }
+        return null;
+    }
+}
