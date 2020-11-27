b7e5bda02aba223619eadb4996b98fc9dbfb4f17
林邦泉 <linbq@techsure.com.cn>
2020-11-27 09:43:20 +0800

    --story=1003705 --user=林邦泉 邮件通知添加流程结束通知用户评分和评分后通知处理人 https://www.tapd.cn/54247054/s/1109808

---
+++ src/main/java/codedriver/framework/process/dto/score/ProcessTaskAutoScoreVo.java
@@ -0,0 +1,28 @@
+package codedriver.framework.process.dto.score;
+
+import java.util.Date;
+
+public class ProcessTaskAutoScoreVo {
+
+    public Long processTaskId;
+    public Date triggerTime;
+    public String config;
+    public Long getProcessTaskId() {
+        return processTaskId;
+    }
+    public void setProcessTaskId(Long processTaskId) {
+        this.processTaskId = processTaskId;
+    }
+    public Date getTriggerTime() {
+        return triggerTime;
+    }
+    public void setTriggerTime(Date triggerTime) {
+        this.triggerTime = triggerTime;
+    }
+    public String getConfig() {
+        return config;
+    }
+    public void setConfig(String config) {
+        this.config = config;
+    }
+}
