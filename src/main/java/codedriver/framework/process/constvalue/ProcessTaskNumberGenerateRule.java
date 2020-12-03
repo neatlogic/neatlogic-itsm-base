40b44865533514a5bb7ad797ce7cd9c29f3586d3
林邦泉 <linbq@techsure.com.cn>
2020-12-03 14:18:33 +0800

    --story=1003752 --user=林邦泉 用户自定义工单号规配置功能 https://www.tapd.cn/54247054/s/1112444

--- src/main/java/codedriver/framework/process/constvalue/ProcessTaskNumberGenerateRule.java
+++ src/main/java/codedriver/framework/process/constvalue/ProcessTaskNumberGenerateRule.java
@@ -1,7 +1,9 @@
 package codedriver.framework.process.constvalue;
 
 public enum ProcessTaskNumberGenerateRule {
-    AUTO_INCREMENT("autoincrement", "自增序列"), DATETIME_AUTO_INCREMENT("datetimeautoincrement", "日期+自增序列");
+    AUTO_INCREMENT("autoincrement", "自增序列"), 
+    DATETIME_AUTO_INCREMENT("datetimeautoincrement", "日期+自增序列");
+    
     private String value;
     private String text;
     private ProcessTaskNumberGenerateRule(String value, String text) {
