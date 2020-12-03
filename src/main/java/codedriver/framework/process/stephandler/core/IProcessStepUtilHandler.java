5c9541cf47ff0db1c31958a46bdd62a4801300e0
林邦泉 <linbq@techsure.com.cn>
2020-12-03 16:20:59 +0800

    --bug=1006471 --user=林邦泉 上报节点描述必填不生效 https://www.tapd.cn/54247054/s/1112651

--- src/main/java/codedriver/framework/process/stephandler/core/IProcessStepUtilHandler.java
+++ src/main/java/codedriver/framework/process/stephandler/core/IProcessStepUtilHandler.java
@@ -16,79 +16,84 @@ import codedriver.framework.process.stepremind.core.IProcessTaskStepRemindType;
 
 public interface IProcessStepUtilHandler {
 
-	public String getHandler();
+    public String getHandler();
 
-	/**
-	 * 
-	* @Time:2020年7月27日
-	* @Description: 处理器特有的步骤信息
-	* @param @return 
-	* @return Object
-	 */
-	public Object getHandlerStepInfo(ProcessTaskStepVo currentProcessTaskStepVo);
-	/**
-	 * 
-	* @Time:2020年8月12日
-	* @Description: 处理器特有的步骤初始化信息
-	* @param @return 
-	* @return Object
-	 */
-	public Object getHandlerStepInitInfo(ProcessTaskStepVo currentProcessTaskStepVo);
-
-	/**
-	 * @Author: chenqiwei
-	 * @Time:Feb 10, 2020
-	 * @Description: 组装步骤节点信息
-	 * @param @param processStepVo
-	 * @param @param stepConfigObj
-	 * @return void
-	 */
-	public void makeupProcessStep(ProcessStepVo processStepVo, JSONObject stepConfigObj);
-	
-	/**
-	 * 
-	 * @Description: 子任务状态发生变化后，对子任务处理人的在
-	 *               processtask_step_worker表和processtask_step_user表的数据做对应的变化
-	 * @param processTaskId
-	 * @param processTaskStepId
-	 * @return void
-	 */
-	public void updateProcessTaskStepUserAndWorker(Long processTaskId, Long processTaskStepId);
-
-	/**
-	 * 
-	 * @Description: 活动审计
-	 * @param currentProcessTaskStepVo
-	 * @param action
-	 * @return void
-	 */
-	public void activityAudit(ProcessTaskStepVo currentProcessTaskStepVo, IProcessTaskAuditType action);
-
-	/**
-	 * 
-	* @Time:2020年8月13日
-	* @Description: 通知 
-	* @param currentProcessTaskStepVo
-	* @param  trigger 
-	* @return void
-	 */
-	public void notify(ProcessTaskStepVo currentProcessTaskStepVo, INotifyTriggerType trigger);
-	/**
-	 * 
-	* @Time:2020年11月11日
-	* @Description: 触发计算sla 
-	* @param currentProcessTaskVo 
-	* @return void
-	 */
-	public void calculateSla(ProcessTaskVo currentProcessTaskVo, boolean isAsync);
-	/**
+    /**
      * 
-    * @Time:2020年11月11日
-    * @Description: 触发计算sla 
-    * @param currentProcessTaskStepVo 
-    * @return void
+     * @Time:2020年7月27日
+     * @Description: 处理器特有的步骤信息
+     * @param @return
+     * @return Object
+     */
+    public Object getHandlerStepInfo(ProcessTaskStepVo currentProcessTaskStepVo);
+
+    /**
+     * 
+     * @Time:2020年8月12日
+     * @Description: 处理器特有的步骤初始化信息
+     * @param @return
+     * @return Object
+     */
+    public Object getHandlerStepInitInfo(ProcessTaskStepVo currentProcessTaskStepVo);
+
+    /**
+     * @Author: chenqiwei
+     * @Time:Feb 10, 2020
+     * @Description: 组装步骤节点信息
+     * @param @param
+     *            processStepVo
+     * @param @param
+     *            stepConfigObj
+     * @return void
+     */
+    public void makeupProcessStep(ProcessStepVo processStepVo, JSONObject stepConfigObj);
+
+    /**
+     * 
+     * @Description: 子任务状态发生变化后，对子任务处理人的在 processtask_step_worker表和processtask_step_user表的数据做对应的变化
+     * @param processTaskId
+     * @param processTaskStepId
+     * @return void
+     */
+    public void updateProcessTaskStepUserAndWorker(Long processTaskId, Long processTaskStepId);
+
+    /**
+     * 
+     * @Description: 活动审计
+     * @param currentProcessTaskStepVo
+     * @param action
+     * @return void
+     */
+    public void activityAudit(ProcessTaskStepVo currentProcessTaskStepVo, IProcessTaskAuditType action);
+
+    /**
+     * 
+     * @Time:2020年8月13日
+     * @Description: 通知
+     * @param currentProcessTaskStepVo
+     * @param trigger
+     * @return void
+     */
+    public void notify(ProcessTaskStepVo currentProcessTaskStepVo, INotifyTriggerType trigger);
+
+    /**
+     * 
+     * @Time:2020年11月11日
+     * @Description: 触发计算sla
+     * @param currentProcessTaskVo
+     * @return void
+     */
+    public void calculateSla(ProcessTaskVo currentProcessTaskVo, boolean isAsync);
+
+    /**
+     * 
+     * @Time:2020年11月11日
+     * @Description: 触发计算sla
+     * @param currentProcessTaskStepVo
+     * @return void
      */
     public void calculateSla(ProcessTaskStepVo currentProcessTaskStepVo);
+
     /**
      * 
      * @Time:2020年3月30日
@@ -98,6 +103,7 @@ public interface IProcessStepUtilHandler {
      * @return List<String>
      */
     public List<ProcessTaskOperationType> getOperateList(Long processTaskId, Long processTaskStepId);
+
     /**
      * 
      * @Time:2020年3月30日
@@ -106,7 +112,9 @@ public interface IProcessStepUtilHandler {
      * @param processTaskStepId
      * @return List<String>
      */
-    public List<ProcessTaskOperationType> getOperateList(ProcessTaskVo processTaskVo, ProcessTaskStepVo processTaskStepVo);
+    public List<ProcessTaskOperationType> getOperateList(ProcessTaskVo processTaskVo,
+        ProcessTaskStepVo processTaskStepVo);
+
     /**
      * 
      * @Time:2020年3月30日
@@ -115,7 +123,9 @@ public interface IProcessStepUtilHandler {
      * @param processTaskStepId
      * @return List<String>
      */
-    public List<ProcessTaskOperationType> getOperateList(Long processTaskId, Long processTaskStepId, List<ProcessTaskOperationType> operationTypeList);
+    public List<ProcessTaskOperationType> getOperateList(Long processTaskId, Long processTaskStepId,
+        List<ProcessTaskOperationType> operationTypeList);
+
     /**
      * 
      * @Time:2020年3月30日
@@ -124,37 +134,9 @@ public interface IProcessStepUtilHandler {
      * @param processTaskStepId
      * @return List<String>
      */
-    public List<ProcessTaskOperationType> getOperateList(ProcessTaskVo processTaskVo, ProcessTaskStepVo processTaskStepVo, List<ProcessTaskOperationType> operationTypeList);
-	/**
-	 * 
-	 * @Time:2020年3月30日
-	 * @Description: 判断当前用户对当前步骤的某个操作是否有权限
-	 * @param processTaskId
-	 * @param processTaskStepId
-	 * @param action
-	 * @return boolean
-	 */
-	public boolean verifyOperationAuthoriy(Long processTaskId, Long processTaskStepId, ProcessTaskOperationType operationType, boolean isThrowException);
-	/**
-     * 
-     * @Time:2020年3月30日
-     * @Description: 判断当前用户对当前步骤的某个操作是否有权限
-     * @param processTaskId
-     * @param processTaskStepId
-     * @param action
-     * @return boolean
-     */
-    public boolean verifyOperationAuthoriy(ProcessTaskVo processTaskVo, ProcessTaskStepVo processTaskStepVo, ProcessTaskOperationType operationType, boolean isThrowException);
-	/**
-     * 
-     * @Time:2020年3月30日
-     * @Description: 判断当前用户对当前步骤的某个操作是否有权限
-     * @param processTaskId
-     * @param processTaskStepId
-     * @param action
-     * @return boolean
-     */
-    public boolean verifyOperationAuthoriy(Long processTaskId, ProcessTaskOperationType operationType, boolean isThrowException);
+    public List<ProcessTaskOperationType> getOperateList(ProcessTaskVo processTaskVo,
+        ProcessTaskStepVo processTaskStepVo, List<ProcessTaskOperationType> operationTypeList);
+
     /**
      * 
      * @Time:2020年3月30日
@@ -164,85 +146,155 @@ public interface IProcessStepUtilHandler {
      * @param action
      * @return boolean
      */
-    public boolean verifyOperationAuthoriy(ProcessTaskVo processTaskVo, ProcessTaskOperationType operationType, boolean isThrowException);
-    
-	/**
-	 * 
-	* @Time:2020年6月30日
-	* @Description: 构造节点管理配置数据
-	* @param configObj
-	* @return JSONObject
-	 */
-	public JSONObject makeupConfig(JSONObject configObj);
-	/**
-     * 
-    * @Time:2020年8月21日
-    * @Description: 获取工单信息 
-    * @param processTaskId 工单id
-    * @return ProcessTaskVo
-     */
-    public ProcessTaskVo getProcessTaskDetailById(Long processTaskId);   
+    public boolean verifyOperationAuthoriy(Long processTaskId, Long processTaskStepId,
+        ProcessTaskOperationType operationType, boolean isThrowException);
+
     /**
      * 
-    * @Time:2020年7月3日
-    * @Description: 获取所有工单干系人信息，用于通知接收人
-    * @param processTaskId 工单id
-    * @param processTaskStepId 步骤id
-    * @param receiverMap 工单干系人信息
-    * @return void
+     * @Time:2020年3月30日
+     * @Description: 判断当前用户对当前步骤的某个操作是否有权限
+     * @param processTaskId
+     * @param processTaskStepId
+     * @param action
+     * @return boolean
      */
-    public void getReceiverMap(ProcessTaskStepVo currentProcessTaskStepVo, Map<String, List<NotifyReceiverVo>> receiverMap);
-    
+    public boolean verifyOperationAuthoriy(ProcessTaskVo processTaskVo, ProcessTaskStepVo processTaskStepVo,
+        ProcessTaskOperationType operationType, boolean isThrowException);
+
     /**
      * 
-    * @Time:2020年9月15日
-    * @Description: 根据工单步骤id获取自定义按钮文案映射 
-    * @param processTaskStepId 
-    * @return void
+     * @Time:2020年3月30日
+     * @Description: 判断当前用户对当前步骤的某个操作是否有权限
+     * @param processTaskId
+     * @param processTaskStepId
+     * @param action
+     * @return boolean
+     */
+    public boolean verifyOperationAuthoriy(Long processTaskId, ProcessTaskOperationType operationType,
+        boolean isThrowException);
+
+    /**
+     * 
+     * @Time:2020年3月30日
+     * @Description: 判断当前用户对当前步骤的某个操作是否有权限
+     * @param processTaskId
+     * @param processTaskStepId
+     * @param action
+     * @return boolean
+     */
+    public boolean verifyOperationAuthoriy(ProcessTaskVo processTaskVo, ProcessTaskOperationType operationType,
+        boolean isThrowException);
+
+    /**
+     * 
+     * @Time:2020年6月30日
+     * @Description: 构造节点管理配置数据
+     * @param configObj
+     * @return JSONObject
+     */
+    public JSONObject makeupConfig(JSONObject configObj);
+
+    /**
+     * 
+     * @Time:2020年8月21日
+     * @Description: 获取工单信息
+     * @param processTaskId
+     *            工单id
+     * @return ProcessTaskVo
+     */
+    public ProcessTaskVo getProcessTaskDetailById(Long processTaskId);
+
+    /**
+     * 
+     * @Time:2020年8月21日
+     * @Description: 获取开始步骤信息
+     * @param processTaskId
+     *            工单id
+     * @return ProcessTaskStepVo
+     */
+    public ProcessTaskStepVo getStartProcessTaskStepByProcessTaskId(Long processTaskId);
+
+    /**
+     * 
+     * @Time:2020年12月3日
+     * @Description: 获取来源工单信息
+     * @param processTaskId
+     * @return ProcessTaskVo
+     */
+    public ProcessTaskVo getFromProcessTasById(Long processTaskId);
+
+    /**
+     * 
+     * @Time:2020年7月3日
+     * @Description: 获取所有工单干系人信息，用于通知接收人
+     * @param processTaskId
+     *            工单id
+     * @param processTaskStepId
+     *            步骤id
+     * @param receiverMap
+     *            工单干系人信息
+     * @return void
+     */
+    public void getReceiverMap(ProcessTaskStepVo currentProcessTaskStepVo,
+        Map<String, List<NotifyReceiverVo>> receiverMap);
+
+    /**
+     * 
+     * @Time:2020年9月15日
+     * @Description: 根据工单步骤id获取自定义按钮文案映射
+     * @param processTaskStepId
+     * @return void
      */
     public Map<String, String> getCustomButtonMapByProcessTaskStepId(Long processTaskStepId);
+
     /**
      * 
-    * @Time:2020年9月15日
-    * @Description: 根据步骤configHash和handler获取自定义按钮文案映射 
-    * @param processTaskStepId 
-    * @return void
+     * @Time:2020年9月15日
+     * @Description: 根据步骤configHash和handler获取自定义按钮文案映射
+     * @param processTaskStepId
+     * @return void
      */
     public Map<String, String> getCustomButtonMapByConfigHashAndHandler(String configHash, String handler);
+
     /**
      * 
-    * @Time:2020年9月15日
-    * @Description: 根据步骤configHash和handler、status获取自定义按钮文案
-    * @param processTaskStepId 
-    * @return void
+     * @Time:2020年9月15日
+     * @Description: 根据步骤configHash和handler、status获取自定义按钮文案
+     * @param processTaskStepId
+     * @return void
      */
     public String getStatusTextByConfigHashAndHandler(String configHash, String handler, String status);
-    
+
     /**
      * 
-    * @Time:2020年9月22日
-    * @Description: 保存步骤提醒 
-    * @param @param currentProcessTaskStepVo
-    * @param @param content
-    * @param @param type 
-    * @return void
+     * @Time:2020年9月22日
+     * @Description: 保存步骤提醒
+     * @param @param
+     *            currentProcessTaskStepVo
+     * @param @param
+     *            content
+     * @param @param
+     *            type
+     * @return void
      */
-    public int saveStepRemind(ProcessTaskStepVo currentProcessTaskStepVo,Long targerProcessTaskStepId, String reason, IProcessTaskStepRemindType ation);
-    
+    public int saveStepRemind(ProcessTaskStepVo currentProcessTaskStepVo, Long targerProcessTaskStepId, String reason,
+        IProcessTaskStepRemindType ation);
+
     /**
      * 
-    * @Time:2020年11月23日
-    * @Description: 获取步骤配置信息中isRequired(回复是否必填)字段值 
-    * @param configHash
-    * @return Integer
+     * @Time:2020年11月23日
+     * @Description: 获取步骤配置信息中isRequired(回复是否必填)字段值
+     * @param configHash
+     * @return Integer
      */
     public Integer getIsRequiredByConfigHash(String configHash);
+
     /**
      * 
-    * @Time:2020年11月23日
-    * @Description: 获取步骤配置信息中isNeedContent(回复是否启用)字段值 
-    * @param configHash
-    * @return Integer
+     * @Time:2020年11月23日
+     * @Description: 获取步骤配置信息中isNeedContent(回复是否启用)字段值
+     * @param configHash
+     * @return Integer
      */
     public Integer getIsNeedContentByConfigHash(String configHash);
 }
