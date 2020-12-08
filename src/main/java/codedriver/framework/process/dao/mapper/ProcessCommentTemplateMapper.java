6ee708d7ba4cfc06697ce5d9beea1eb3f6f388a6
Aienao <laiwt@techsure.com.cn>
2020-12-08 17:48:06 +0800

    --story=1003753 --user=赖文韬 用户自定义回复模板功... https://www.tapd.cn/54247054/s/1113779 【使用计数】

--- src/main/java/codedriver/framework/process/dao/mapper/ProcessCommentTemplateMapper.java
+++ src/main/java/codedriver/framework/process/dao/mapper/ProcessCommentTemplateMapper.java
@@ -2,6 +2,7 @@ package codedriver.framework.process.dao.mapper;
 
 import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.process.dto.ProcessCommentTemplateAuthVo;
+import codedriver.framework.process.dto.ProcessCommentTemplateUseCountVo;
 import codedriver.framework.process.dto.ProcessCommentTemplateVo;
 import org.apache.ibatis.annotations.Param;
 
@@ -25,11 +26,17 @@ public interface ProcessCommentTemplateMapper {
 
     public ProcessCommentTemplateVo getTemplateByStepUuidAndAuth(@Param("stepUuid") String uuid,@Param("authList") List<String> authList);
 
+    public ProcessCommentTemplateUseCountVo getTemplateUseCount(@Param("templateId") Long id, @Param("userUuid") String uuid);
+
     public int updateTemplate(ProcessCommentTemplateVo vo);
+
+    public int updateTemplateUseCount(@Param("templateId") Long id, @Param("userUuid") String uuid);
 
     public int insertTemplate(ProcessCommentTemplateVo vo);
 
     public int batchInsertAuthority(List<ProcessCommentTemplateAuthVo> list);
+
+    public int insertTemplateUseCount(@Param("templateId") Long id, @Param("userUuid") String uuid);
 
     public int deleteTemplate(Long id);
 
