47c8e40757c937e0d0d0e2e476bf3770b935b8c7
Aienao <laiwt@techsure.com.cn>
2021-02-22 17:46:16 +0800

    --story=1004033 --user=赖文韬 校验提示样式统一 https://www.tapd.cn/54247054/s/1136286

--- src/main/java/codedriver/framework/process/exception/channeltype/ChannelTypeRelationNameRepeatException.java
+++ src/main/java/codedriver/framework/process/exception/channeltype/ChannelTypeRelationNameRepeatException.java
@@ -1,13 +1,13 @@
 package codedriver.framework.process.exception.channeltype;
 
-import codedriver.framework.exception.core.ApiRuntimeException;
+import codedriver.framework.exception.core.ApiFieldValidRuntimeException;
 
-public class ChannelTypeRelationNameRepeatException extends ApiRuntimeException {
+public class ChannelTypeRelationNameRepeatException extends ApiFieldValidRuntimeException {
 
     private static final long serialVersionUID = -5875667147910092453L;
     
     public ChannelTypeRelationNameRepeatException(String name) {
-        super("服务类型关系名称：'" + name + "'已存在");
+        super("服务类型关系：'" + name + "'已存在");
     }
 
 }
--- src/main/java/codedriver/framework/process/exception/score/ScoreTemplateNameRepeatException.java
+++ src/main/java/codedriver/framework/process/exception/score/ScoreTemplateNameRepeatException.java
@@ -1,12 +1,12 @@
 package codedriver.framework.process.exception.score;
 
-import codedriver.framework.process.exception.core.ProcessTaskRuntimeException;
+import codedriver.framework.exception.core.ApiFieldValidRuntimeException;
 
-public class ScoreTemplateNameRepeatException extends ProcessTaskRuntimeException {
+public class ScoreTemplateNameRepeatException extends ApiFieldValidRuntimeException {
 
 	private static final long serialVersionUID = 7647275192524314055L;
 
 	public ScoreTemplateNameRepeatException(String name) {
-		super("评分模版名 :" + name + "已存在");
+		super("评分模版 :" + name + "已存在");
 	}
 }
