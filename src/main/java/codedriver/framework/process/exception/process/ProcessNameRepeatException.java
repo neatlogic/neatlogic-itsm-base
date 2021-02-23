0038f221e24757d19995873089e1bc41e2daf6d8
Aienao <laiwt@techsure.com.cn>
2021-02-23 11:04:28 +0800

    --story=1004033 --user=赖文韬 校验提示样式统一 https://www.tapd.cn/54247054/s/1136414

--- src/main/java/codedriver/framework/process/exception/process/ProcessNameRepeatException.java
+++ src/main/java/codedriver/framework/process/exception/process/ProcessNameRepeatException.java
@@ -1,12 +1,12 @@
 package codedriver.framework.process.exception.process;
 
-import codedriver.framework.process.exception.core.ProcessTaskRuntimeException;
+import codedriver.framework.exception.core.ApiFieldValidRuntimeException;
 
-public class ProcessNameRepeatException extends ProcessTaskRuntimeException {
+public class ProcessNameRepeatException extends ApiFieldValidRuntimeException {
 
 	private static final long serialVersionUID = -4617724920030245149L;
 
 	public ProcessNameRepeatException(String msg) {
-		super("流程图名称：'" + msg + "'已存在");
+		super("流程图：'" + msg + "'已存在");
 	}
 }
