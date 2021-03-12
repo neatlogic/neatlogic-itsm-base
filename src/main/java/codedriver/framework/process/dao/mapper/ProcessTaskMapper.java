209cab52d557ee1e71e996ac870705329bbebb72
Aienao <laiwt@techsure.com.cn>
2021-03-12 16:16:09 +0800

    --bug=1007549 --user=赖文韬 定时任务-发送的定时消... https://www.tapd.cn/54247054/s/1142179

--- src/main/java/codedriver/framework/process/dao/mapper/ProcessTaskMapper.java
+++ src/main/java/codedriver/framework/process/dao/mapper/ProcessTaskMapper.java
@@ -36,14 +36,17 @@ public interface ProcessTaskMapper {
 
     public ProcessTaskVo getProcessTaskBaseInfoById(Long processTaskId);
 
+
+    public List<ProcessTaskVo> getTaskListByIdList(List<Long> idList);
+
     /**
-     * @Description: 查询待处理的工单
+     * @Description: 查询待处理的工单id
      * @Author: laiwt
      * @Date: 2021/1/11 12:01
      * @Params: [map]
      * @Returns: java.util.List<codedriver.framework.process.dto.ProcessTaskVo>
      **/
-    public List<ProcessTaskVo> getProcessingTaskListByCondition(@Param("conditionMap") Map<String,Object> map);
+    public List<Long> getProcessingTaskIdListByCondition(@Param("conditionMap") Map<String,Object> map);
 
     public List<ProcessTaskStepVo> getProcessTaskStepBaseInfoByProcessTaskId(Long processTaskId);
 
