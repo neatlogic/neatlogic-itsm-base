de8fc14e3e21fce2a33745f631b6c691b5ea2336
chenqiwei <chenqw@techsure.com.cn>
2021-03-16 09:44:25 +0800

    优化
    Signed-off-by: chenqiwei <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/process/audithandler/core/IProcessTaskAuditType.java
+++ src/main/java/codedriver/framework/process/audithandler/core/IProcessTaskAuditType.java
@@ -1,14 +1,16 @@
 package codedriver.framework.process.audithandler.core;
+
 /**
- * 
-* @Time:2020年8月11日
-* @ClassName: IProcessTaskAuditType 
-* @Description: 工单活动类型接口
+ * @Time: 2020年8月11日
+ * @ClassName: IProcessTaskAuditType
+ * @Description: 工单活动类型接口
  */
 public interface IProcessTaskAuditType {
-	
-	public String getValue();
-	public String getText();
-	public String getDescription();
-	
+
+    String getValue();
+
+    String getText();
+
+    String getDescription();
+
 }
--- src/main/java/codedriver/framework/process/dto/AttributeDataVo.java
+++ src/main/java/codedriver/framework/process/dto/AttributeDataVo.java
@@ -72,23 +72,27 @@ public class AttributeDataVo {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		AttributeDataVo other = (AttributeDataVo) obj;
 		if (attributeUuid == null) {
-			if (other.attributeUuid != null)
+			if (other.attributeUuid != null) {
 				return false;
-		} else if (!attributeUuid.equals(other.attributeUuid))
+			}
+		} else if (!attributeUuid.equals(other.attributeUuid)) {
 			return false;
+		}
 		if (data == null) {
-			if (other.data != null)
-				return false;
-		} else if (!data.equals(other.data))
-			return false;
-		return true;
+			return other.data == null;
+		} else {
+			return data.equals(other.data);
+		}
 	}
 }
