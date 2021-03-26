15e1a2b620b12ab35105dc39902507585620e0ce
Aienao <laiwt@techsure.com.cn>
2021-03-26 17:30:22 +0800

    --bug=1007500 --user=赖文韬 服务窗口管理-被引用的... https://www.tapd.cn/54247054/s/1146294

--- src/main/java/codedriver/framework/process/exception/worktime/WorktimeHasBeenRelatedByChannelException.java
+++ src/main/java/codedriver/framework/process/exception/worktime/WorktimeHasBeenRelatedByChannelException.java
@@ -1,10 +1,4 @@
-/*
- * Copyright (c) 2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- *
+/**
  * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
