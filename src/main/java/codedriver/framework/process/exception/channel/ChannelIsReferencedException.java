28644c28b1087a54b4614af74b298b0fc291956f
林邦泉 <linbq@techsure.com.cn>
2021-03-30 15:01:48 +0800

    --story=1004281 --user=林邦泉 服务的删除逻辑调整 https://www.tapd.cn/54247054/s/1147073

---
+++ src/main/java/codedriver/framework/process/exception/channel/ChannelIsReferencedException.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.process.exception.channel;
+
+import codedriver.framework.exception.core.ApiRuntimeException;
+
+/**
+ * @author: linbq
+ * @since: 2021/3/30 12:12
+ **/
+public class ChannelIsReferencedException extends ApiRuntimeException {
+
+    private static final long serialVersionUID = 291889732332376765L;
+
+    public ChannelIsReferencedException(String channelName){
+        super("服务：'" + channelName + "'已被引用");
+    }
+}
