d3bcd171c0db37c22ee2d45a61e1ffdb10008cb3
Aienao <laiwt@techsure.com.cn>
2021-04-07 19:16:51 +0800

    --bug=1007812 --user=赖文韬 批量上报-批量上报失败... https://www.tapd.cn/54247054/s/1148345

---
+++ src/main/java/codedriver/framework/process/exception/core/ProcessTaskPriorityNotMatchException.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.process.exception.core;
+
+import codedriver.framework.exception.core.ApiRuntimeException;
+
+public class ProcessTaskPriorityNotMatchException extends ApiRuntimeException {
+
+	private static final long serialVersionUID = -5766752850987530449L;
+
+	public ProcessTaskPriorityNotMatchException() {
+		super("工单优先级与当前服务优先级不匹配");
+	}
+
+}
