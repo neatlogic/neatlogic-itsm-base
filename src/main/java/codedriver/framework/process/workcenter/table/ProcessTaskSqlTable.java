ecc16c7abfe7b0c83f39978d4413fa2e97dc493d
吕佐康 <lvzk@techsure.com.cn>
2021-04-22 18:05:54 +0800

    bug fixed

--- src/main/java/codedriver/framework/process/workcenter/table/ProcessTaskSqlTable.java
+++ src/main/java/codedriver/framework/process/workcenter/table/ProcessTaskSqlTable.java
@@ -40,7 +40,7 @@ public class ProcessTaskSqlTable implements ISqlTable {
         END_TIME("end_time", "结束时间"),
         OWNER("owner", "上报人"),
         REPORTER("reporter", "上报人"),
-        STATUS("status", "工单状态","statusName","status"),
+        STATUS("status", "工单状态","status","status"),
         PRIORITY_UUID("priority_uuid", "工单状态"),
         CHANNEL_UUID("channel_uuid", "工单状态"),
         IS_SHOW("is_show", "工单是否隐藏"),
