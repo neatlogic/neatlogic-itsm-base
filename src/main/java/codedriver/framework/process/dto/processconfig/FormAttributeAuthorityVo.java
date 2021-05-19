e09478ad7355c3e4188610a0f0d16f0876033e97
林邦泉 <linbq@techsure.com.cn>
2021-05-19 18:57:26 +0800

    --story=1004669 --user=林邦泉 流程修复旧配置数据&校验数据合法性 https://www.tapd.cn/54247054/s/1155581

---
+++ src/main/java/codedriver/framework/process/dto/processconfig/FormAttributeAuthorityVo.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.process.dto.processconfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author linbq
+ * @since 2021/5/19 17:55
+ **/
+public class FormAttributeAuthorityVo {
+    private String action = "";
+    private String component = "";
+    private List<String> attributeUuidList = new ArrayList<>();
+    private List<String> processStepUuidList = new ArrayList<>();
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public List<String> getAttributeUuidList() {
+        return attributeUuidList;
+    }
+
+    public void setAttributeUuidList(List<String> attributeUuidList) {
+        this.attributeUuidList = attributeUuidList;
+    }
+
+    public List<String> getProcessStepUuidList() {
+        return processStepUuidList;
+    }
+
+    public void setProcessStepUuidList(List<String> processStepUuidList) {
+        this.processStepUuidList = processStepUuidList;
+    }
+}
