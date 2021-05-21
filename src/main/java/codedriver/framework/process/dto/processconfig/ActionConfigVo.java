e4d4f4df3af3008f3221e67b5b2f51f7767d3bb1
林邦泉 <linbq@techsure.com.cn>
2021-05-21 18:36:51 +0800

    --story=1004669 --user=林邦泉 流程修复旧配置数据&校验数据合法性 https://www.tapd.cn/54247054/s/1155839

--- src/main/java/codedriver/framework/process/dto/processconfig/ActionConfigVo.java
+++ src/main/java/codedriver/framework/process/dto/processconfig/ActionConfigVo.java
@@ -13,7 +13,7 @@ import java.util.List;
  **/
 public class ActionConfigVo {
     private String handler;
-    private String integrationHandler;
+    private String integrationHandler = "codedriver.module.process.integration.handler.ProcessActionIntegrationHandler";
     private List<ActionConfigActionVo> actionList;
 
     public String getHandler() {
