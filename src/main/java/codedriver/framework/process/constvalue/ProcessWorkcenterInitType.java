855ab29d10ee4d3170dda070ae54083f4dd013dc
吕佐康 <lvzk@techsure.com.cn>
2021-09-06 12:13:30 +0800

    --task=1005249 --user=lvzk 工单中心-出厂分类支持授权-后端 https://www.tapd.cn/54247054/s/1176034

---
+++ src/main/java/codedriver/framework/process/constvalue/ProcessWorkcenterInitType.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c)  2021 TechSure Co.,Ltd.  All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.process.constvalue;
+
+/**
+ * @author lvzk
+ * @since 2021/9/6 11:55
+ **/
+public enum ProcessWorkcenterInitType {
+    ALL_PROCESSTASK("allProcessTask", "默认出厂"), DRAFT_PROCESSTASK("draftProcessTask", "系统分类");
+    private final String value;
+    private final String name;
+
+    private ProcessWorkcenterInitType(String _value, String _name) {
+        this.value = _value;
+        this.name = _name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String getValue(String _value) {
+        for (ProcessWorkcenterInitType s : ProcessWorkcenterInitType.values()) {
+            if (s.getValue().equals(_value)) {
+                return s.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static String getName(String _value) {
+        for (ProcessWorkcenterInitType s : ProcessWorkcenterInitType.values()) {
+            if (s.getValue().equals(_value)) {
+                return s.getName();
+            }
+        }
+        return "";
+    }
+}
