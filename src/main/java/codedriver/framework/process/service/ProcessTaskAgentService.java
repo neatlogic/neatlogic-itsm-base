e7c510582fd1f78499acae6bbcfba5134efd6565
林邦泉 <linbq@techsure.com.cn>
2021-10-11 16:46:06 +0800

    --task=1005370 --user=林邦泉 后端-苏州银行-任务临时授权功能优化 https://www.tapd.cn/54247054/s/1181872

---
+++ src/main/java/codedriver/framework/process/service/ProcessTaskAgentService.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.process.service;
+
+/**
+ * @author linbq
+ * @since 2021/10/11 15:50
+ **/
+public interface ProcessTaskAgentService {
+}
---
+++ src/main/java/codedriver/framework/process/service/ProcessTaskAgentServiceImpl.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.process.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author linbq
+ * @since 2021/10/11 15:51
+ **/
+@Service
+public class ProcessTaskAgentServiceImpl implements ProcessTaskAgentService {
+}
