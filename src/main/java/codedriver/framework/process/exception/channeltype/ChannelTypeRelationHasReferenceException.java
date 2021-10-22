7ebf88463f7c82e317f84a398d2a2180ff148c11
陈其炜 <chenqw@techsure.com.cn>
2021-10-22 19:26:15 +0800

    增加属性和关系修改禁用功能
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/process/exception/channeltype/ChannelTypeRelationHasReferenceException.java
+++ src/main/java/codedriver/framework/process/exception/channeltype/ChannelTypeRelationHasReferenceException.java
@@ -1,21 +1,17 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.process.exception.channeltype;
 
 import codedriver.framework.exception.core.ApiRuntimeException;
 
-/**
- * @Title: ChannelTypeRelationHasReferenceException
- * @Package codedriver.framework.process.exception.channeltype
- * @Description: 服务类型关系已被引用异常
- * @Author: linbq
- * @Date: 2021/1/31 15:34
- * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
- * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
- **/
 public class ChannelTypeRelationHasReferenceException extends ApiRuntimeException {
 
     private static final long serialVersionUID = 2411827472270844447L;
 
     public ChannelTypeRelationHasReferenceException(String name) {
-        super("服务类型关系：'" + name + "'已被引用，不能修改");
+        super("服务类型关系“" + name + "”已被引用，不能修改");
     }
 }
--- src/main/java/codedriver/framework/process/exception/channeltype/ChannelTypeRelationNameRepeatException.java
+++ src/main/java/codedriver/framework/process/exception/channeltype/ChannelTypeRelationNameRepeatException.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.process.exception.channeltype;
 
 import codedriver.framework.exception.core.ApiRuntimeException;
@@ -7,7 +12,7 @@ public class ChannelTypeRelationNameRepeatException extends ApiRuntimeException 
     private static final long serialVersionUID = -5875667147910092453L;
     
     public ChannelTypeRelationNameRepeatException(String name) {
-        super("服务类型关系：'" + name + "'已存在");
+        super("服务类型关系“" + name + "”已存在");
     }
 
 }
