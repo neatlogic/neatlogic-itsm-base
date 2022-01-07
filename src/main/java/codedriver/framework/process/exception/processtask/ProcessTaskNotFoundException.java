cb645d05dc73efb582d81907abd3832df11890b2
longrf <techsure901>
2022-01-07 12:07:20 +0800

    --story=1005724 --user=龙儒锋 附件下载权限认证-change、event https://www.tapd.cn/54247054/s/1201529

--- src/main/java/codedriver/framework/process/exception/processtask/ProcessTaskNotFoundException.java
+++ src/main/java/codedriver/framework/process/exception/processtask/ProcessTaskNotFoundException.java
@@ -4,11 +4,11 @@ import codedriver.framework.process.exception.core.ProcessTaskRuntimeException;
 
 public class ProcessTaskNotFoundException extends ProcessTaskRuntimeException {
 
-	private static final long serialVersionUID = 2861954159600811000L;
+    private static final long serialVersionUID = 2861954159600811000L;
 
-	public ProcessTaskNotFoundException(String processTask) {
-		super("工单：'" + processTask + "'不存在");
-	}
+    public ProcessTaskNotFoundException(String processTask) {
+        super("工单：'" + processTask + "'不存在");
+    }
 
     public ProcessTaskNotFoundException() {
         super("工单不存在");
