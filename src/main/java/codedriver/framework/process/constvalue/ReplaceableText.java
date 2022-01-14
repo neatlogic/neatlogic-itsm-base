b59d30a5ee2bdb3adfd33c2b980e85162dc9b656
林邦泉 <linbq@techsure.com.cn>
2022-01-14 15:28:19 +0800

    --task=1005835 --user=林邦泉 后端-苏州银行-工单处理页面，如果当前是审批节点，tab步骤日志 文案改成 审批日志 https://www.tapd.cn/54247054/s/1203436

--- src/main/java/codedriver/framework/process/constvalue/ReplaceableText.java
+++ src/main/java/codedriver/framework/process/constvalue/ReplaceableText.java
@@ -11,10 +11,10 @@ package codedriver.framework.process.constvalue;
  **/
 public enum ReplaceableText {
 //    SUBTASK("replaceable_subtask", "子任务")
-    REPORTCONTENT("replaceable_reportcontent", "上报内容"),
-    STEPLIST("replaceable_steplist", "步骤日志"),
-    AUDITLIST("replaceable_auditlist", "活动日志"),
-    RELATIONLIST("replaceable_relationlist", "关联工单")
+    REPORTCONTENT("replaceableReportcontent", "上报内容"),
+    STEPLIST("replaceableSteplist", "步骤日志"),
+    AUDITLIST("replaceableAuditlist", "活动日志"),
+    RELATIONLIST("replaceableRelationlist", "关联工单")
     ;
     private final String value;
     private final String text;
