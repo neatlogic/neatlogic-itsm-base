b0d423468805aca93462a057745b0be18d1f908d
longrf <techsure901>
2022-01-14 19:43:56 +0800

    --bug=1009169 --user=龙儒锋 工单处理-处理人回退工单到前置节点后，下载工单上报内容的附件异常 https://www.tapd.cn/54247054/s/1203342

--- src/main/java/codedriver/framework/process/dao/mapper/ProcessTaskMapper.java
+++ src/main/java/codedriver/framework/process/dao/mapper/ProcessTaskMapper.java
@@ -258,7 +258,7 @@ public interface ProcessTaskMapper {
 
     List<ProcessTaskStepAutomaticRequestVo> getAllProcessTaskStepAutomaticRequestList();
 
-    List<ProcessTaskStepVo> getProcessTaskStepVoListByFileId(Long fileId);
+    List<ProcessTaskVo> getProcessTaskStepVoListByFileId(Long fileId);
 
     ProcessTaskStepTimerVo getProcessTaskStepTimerById(Long id);
 
