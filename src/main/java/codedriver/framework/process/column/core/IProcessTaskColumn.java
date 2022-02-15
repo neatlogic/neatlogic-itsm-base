596539ab3d7bc3bb685cd39cb692e2bf7becd6bc
lvzk <lvzk@techsure.com.cn>
2022-02-15 12:07:01 +0800

    --story=1005904 --user=lvzk 工单中心显示列增加一个可选的列"工单ID" https://www.tapd.cn/54247054/s/1208144

--- src/main/java/codedriver/framework/process/column/core/IProcessTaskColumn.java
+++ src/main/java/codedriver/framework/process/column/core/IProcessTaskColumn.java
@@ -14,106 +14,66 @@ public interface IProcessTaskColumn {
 
     /**
      * @Description: 字段英文名
-     * @Param:
-     * @return: java.lang.String
      * @Date: 2020/2/2
      */
-    public String getName();
+    String getName();
 
     /**
      * @Description: 字段显示名
-     * @Param:
-     * @return: java.lang.String
      * @Date: 2020/2/2
      */
-    public String getDisplayName();
+    String getDisplayName();
 
     /**
      * @Description: 字段是否允许排序
-     * @Param:
-     * @return: java.lang.String
      * @Date: 2020/2/2
      */
-    public Boolean allowSort();
-
-    /**
-     * @Description: 获取显示值 workcenter
-     * @Param:
-     * @return: java.lang.Object
-     * @Date: 2020/2/2
-     */
-//    public Object getValue(MultiAttrsObject el) throws RuntimeException;
-
-    /**
-     * @Description: 获取显示值 dashboard
-     * @Param:
-     * @return: java.lang.Object
-     * @Date: 2020/2/2
-     */
-//    public Object getValueText(MultiAttrsObject el) throws RuntimeException;
+    Boolean allowSort();
 
     /**
      * @Description: 获取类型
-     * @Param:
-     * @return: java.lang.String
      * @Date: 2020/3/25
      */
-    public String getType();
+    String getType();
 
     /**
      * @Description: 获取展示字段样式
-     * @Param:
-     * @return: java.lang.String
      * @Date: 2020/3/26
      */
-    public String getClassName();
+    String getClassName();
 
     /**
      * @Description: 获取展示字段默认顺序
-     * @Param:
-     * @return: java.lang.String
      * @Date: 2020/3/27
      */
-    public Integer getSort();
+    Integer getSort();
 
 
     /**
      * @Description: 是否显示该字段
-     * @Param:
-     * @return: java.lang.String
      * @Date: 2020/6/5
      */
-    public Boolean getIsShow();
-
-    //Object getMyValue(JSONObject json);
-
-    public Boolean getDisabled();
+    Boolean getIsShow();
 
     /**
-     * @return
-     * @Author 89770
-     * @Time 2020年10月19日
+     * 是否可以控制该字段是否显示
+     * @return 是｜否
+     */
+    Boolean getDisabled();
+
+    /**
+     * @author 89770
+     * @date 2020年10月19日
      * @Description: 是否需要按该字段排序工单数据
-     * @Param
      */
-    public Boolean getIsSort();
+    Boolean getIsSort();
 
     /**
-     * 暂时用来从getValue()返回的数据抽取导出excel时需要的数据
-     *
-     * @param json
-     * @return
-     */
-    //Object getSimpleValue(Object json);
-
-    /**
-     * @return
-     * @Author 89770
-     * @Time 2020年12月3日
+     * @author 89770
+     * @date 2020年12月3日
      * @Description: 导出excel，是否需要导出该字段
-     * @Param
      */
-    public Boolean getIsExport();
+    Boolean getIsExport();
 
     /**
      * @Description: 获取需要关联的表和字段
@@ -122,7 +82,7 @@ public interface IProcessTaskColumn {
      * @Params: []
      * @Returns: java.util.List<codedriver.framework.process.workcenter.dto.JoinTableColumnVo>
      **/
-    public List<JoinTableColumnVo> getJoinTableColumnList();
+    List<JoinTableColumnVo> getJoinTableColumnList();
 
     /**
      * @Description: 主table  用于sort distinct
@@ -131,7 +91,7 @@ public interface IProcessTaskColumn {
      * @Params: []
      * @Returns: codedriver.framework.process.workcenter.table.ISqlTable
      **/
-    public ISqlTable getSortSqlTable();
+    ISqlTable getSortSqlTable();
 
     /**
      * @Description: 主column 用于sort distinct
@@ -140,7 +100,7 @@ public interface IProcessTaskColumn {
      * @Params: []
      * @Returns: java.lang.String
      **/
-    public String getSortSqlColumn(Boolean isColumn);
+    String getSortSqlColumn(Boolean isColumn);
 
     /**
      * @Description: 重新渲染字段
@@ -149,7 +109,7 @@ public interface IProcessTaskColumn {
      * @Params: [processTaskVo]
      * @Returns: java.lang.Object
      **/
-    public Object getValue(ProcessTaskVo processTaskVo);
+    Object getValue(ProcessTaskVo processTaskVo);
 
     /**
      * @Description: 重新渲染字段 (主要获取字符串 用于导出)
@@ -158,7 +118,7 @@ public interface IProcessTaskColumn {
      * @Params: [processTaskVo]
      * @Returns: java.lang.Object
      **/
-    public String getSimpleValue(ProcessTaskVo processTaskVo);
+    String getSimpleValue(ProcessTaskVo processTaskVo);
 
     /**
      * @Description: 获取table 需要 select 出来的 column
@@ -167,7 +127,7 @@ public interface IProcessTaskColumn {
      * @Params: [workcenterVo]
      * @Returns: java.util.Map<codedriver.framework.process.workcenter.table.ISqlTable, java.lang.String>
      **/
-    public List<TableSelectColumnVo> getTableSelectColumn();
+    List<TableSelectColumnVo> getTableSelectColumn();
 
     /**
      * @Description: 获取dashboard 映射关系
@@ -176,7 +136,7 @@ public interface IProcessTaskColumn {
      * @Params: [mapList]
      * @Returns: com.alibaba.fastjson.JSONArray
      **/
-    public void getDashboardDataVo(DashboardDataVo dashboardDataVo, WorkcenterVo workcenterVo, List<Map<String, Object>> mapList);
+    void getDashboardDataVo(DashboardDataVo dashboardDataVo, WorkcenterVo workcenterVo, List<Map<String, Object>> mapList);
 
     /**
      * @Description: 将数据集提取groupList, 用于过滤，并按list 权重排序
