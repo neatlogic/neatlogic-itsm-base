75f709863b4674d1ff3d1bbeefcb5c039a575159
陈其炜 <chenqw@techsure.com.cn>
2022-05-06 16:58:48 +0800

    --bug=1009588 --user=陈其炜 工单中心-另存为新分类、编辑工单分类并保存异常 https://www.tapd.cn/54247054/s/1225352
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/process/exception/workcenter/WorkcenterNotFoundException.java
+++ src/main/java/codedriver/framework/process/exception/workcenter/WorkcenterNotFoundException.java
@@ -9,10 +9,11 @@ import codedriver.framework.process.exception.core.ProcessTaskRuntimeException;
 
 public class WorkcenterNotFoundException extends ProcessTaskRuntimeException {
 
-	private static final long serialVersionUID = -6039692333884409352L;
+    private static final long serialVersionUID = -6039692333884409352L;
 
-	public WorkcenterNotFoundException(String handler) {
-		super("找不到：'" + handler + "' 工单中心分类");
-	}
+    public WorkcenterNotFoundException(String handler) {
+        super("工单中心分类“" + handler + "”不存在");
+    }
+
 
 }
