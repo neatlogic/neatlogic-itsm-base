a14412b353a31c7a377fdb03d4dad625658ae22e
linbq@techsure.com.cn <Besiter789>
2022-06-01 19:20:50 +0800

    --bug=1009714 --user=林邦泉 工单中心-其他用户代子任务处理人完成子任务，原处理人的待办工单列表仍包含子任务所在工单 https://www.tapd.cn/54247054/s/1233000

--- src/main/java/codedriver/framework/process/service/ProcessTaskAgentServiceImpl.java
+++ src/main/java/codedriver/framework/process/service/ProcessTaskAgentServiceImpl.java
@@ -16,7 +16,10 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author linbq
