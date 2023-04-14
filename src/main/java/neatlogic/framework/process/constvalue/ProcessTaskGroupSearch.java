72a694e9df0f039975b40d98e517c5ce46d26955
chenqw <zenkiray@live.com>
2023-04-14 20:05:39 +0800

    issue 管理
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/framework/process/constvalue/ProcessTaskGroupSearch.java
+++ src/main/java/neatlogic/framework/process/constvalue/ProcessTaskGroupSearch.java
@@ -1,20 +1,38 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package neatlogic.framework.process.constvalue;
 
 import neatlogic.framework.util.I18nUtils;
 
 public enum ProcessTaskGroupSearch {
-	PROCESSUSERTYPE("processUserType","enum.process.processtaskgroupsearch.processusertype");
-	
-	private String value;
-	private String text;
+	PROCESSUSERTYPE("processUserType", "enum.process.processtaskgroupsearch.processusertype");
+
+	private final String value;
+	private final String text;
+
 	private ProcessTaskGroupSearch(String value, String text) {
 		this.value = value;
 		this.text = text;
 	}
+
 	public String getValue() {
 		return value;
 	}
-	
+
 	public String getValuePlugin() {
 		return value + "#";
 	}
--- src/main/java/neatlogic/framework/process/constvalue/ProcessUserType.java
+++ src/main/java/neatlogic/framework/process/constvalue/ProcessUserType.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package neatlogic.framework.process.constvalue;
 
 import neatlogic.framework.common.constvalue.IUserType;
@@ -7,71 +23,70 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ProcessUserType implements IUserType {
-	MAJOR("major", "处理人",true), 
-	MINOR("minor", "协助处理人",true),
-//	AGENT("agent","代办人",true),
-	OWNER("owner","上报人",true),
-	REPORTER("reporter","代报人",true),
-	WORKER("worker", "待处理人",true),
-	DEFAULT_WORKER("defaultworker", "异常处理人",false),
-	FOCUS_USER("focususer","关注人",false)
-	;
+    MAJOR("major", "处理人", true),
+    MINOR("minor", "协助处理人", true),
+    //	AGENT("agent","代办人",true),
+    OWNER("owner", "上报人", true),
+    REPORTER("reporter", "代报人", true),
+    WORKER("worker", "待处理人", true),
+    DEFAULT_WORKER("defaultworker", "异常处理人", false),
+    FOCUS_USER("focususer", "关注人", false);
 
-	private String status;
-	private String text;
-	private boolean isShow;
+    private final String status;
+    private final String text;
+    private final boolean isShow;
 
-	private ProcessUserType(String _status, String _text,boolean _isShow) {
-		this.status = _status;
-		this.text = _text;
-		this.isShow = _isShow;
-	}
+    private ProcessUserType(String _status, String _text, boolean _isShow) {
+        this.status = _status;
+        this.text = _text;
+        this.isShow = _isShow;
+    }
 
-	public String getValue() {
-		return status;
-	}
+    public String getValue() {
+        return status;
+    }
 
-	public String getText() {
-		return text;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public boolean getIsShow() {
-		return isShow;
-	}
+    public boolean getIsShow() {
+        return isShow;
+    }
 
-	public static String getValue(String _status) {
-		for (ProcessUserType s : ProcessUserType.values()) {
-			if (s.getValue().equals(_status)) {
-				return s.getValue();
-			}
-		}
-		return null;
-	}
+    public static String getValue(String _status) {
+        for (ProcessUserType s : ProcessUserType.values()) {
+            if (s.getValue().equals(_status)) {
+                return s.getValue();
+            }
+        }
+        return null;
+    }
 
-	public static String getText(String _status) {
-		for (ProcessUserType s : ProcessUserType.values()) {
-			if (s.getValue().equals(_status)) {
-				return s.getText();
-			}
-		}
-		return "";
-	}
+    public static String getText(String _status) {
+        for (ProcessUserType s : ProcessUserType.values()) {
+            if (s.getValue().equals(_status)) {
+                return s.getText();
+            }
+        }
+        return "";
+    }
 
 
-	@Override
-	public UserTypeVo getUserType() {
-		UserTypeVo vo = new UserTypeVo();
-		vo.setModuleId(getModuleId());
-		Map<String,String> map = new HashMap<>();
-		for(ProcessUserType type : ProcessUserType.values()){
-			map.put(type.getValue(),type.getText());
-		}
-		vo.setValues(map);
-		return vo;
-	}
+    @Override
+    public UserTypeVo getUserType() {
+        UserTypeVo vo = new UserTypeVo();
+        vo.setModuleId(getModuleId());
+        Map<String,String> map = new HashMap<>();
+        for(ProcessUserType type : ProcessUserType.values()){
+            map.put(type.getValue(),type.getText());
+        }
+        vo.setValues(map);
+        return vo;
+    }
 
-	@Override
-	public String getModuleId() {
-		return "process";
-	}
+    @Override
+    public String getModuleId() {
+        return "process";
+    }
 }
