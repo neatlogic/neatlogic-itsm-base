1aba1c71509462460828d7d26a7cbe46f677940c
chenqw <zenkiray@live.com>
2023-06-27 18:48:52 +0800

    - [修复]代码优化
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/framework/process/audithandler/core/ProcessTaskAuditDetailTypeFactory.java
+++ src/main/java/neatlogic/framework/process/audithandler/core/ProcessTaskAuditDetailTypeFactory.java
@@ -1,22 +1,38 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package neatlogic.framework.process.audithandler.core;
 
+import org.reflections.Reflections;
+
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.reflections.Reflections;
 /**
- * 
-* @Time:2020年8月11日
-* @ClassName: ProcessTaskAuditDetailTypeFactory 
-* @Description: 工单活动内容类型工厂类
+ * @ClassName: ProcessTaskAuditDetailTypeFactory
+ * @Description: 工单活动内容类型工厂类
  */
 public class ProcessTaskAuditDetailTypeFactory {
 	/** 标记是否未初始化数据，只初始化一次 **/
 	private static volatile boolean isUninitialized = true;
 	
-	private static Set<IProcessTaskAuditDetailType> set = new HashSet<>();
-	
-	public static Set<IProcessTaskAuditDetailType> getAuditDetailTypeList(){		
+	private static final Set<IProcessTaskAuditDetailType> set = new HashSet<>();
+
+    public static Set<IProcessTaskAuditDetailType> getAuditDetailTypeList(){
 		if(isUninitialized) {
 			synchronized(ProcessTaskAuditDetailTypeFactory.class) {
 				if(isUninitialized) {
@@ -24,13 +40,11 @@ public class ProcessTaskAuditDetailTypeFactory {
 					Set<Class<? extends IProcessTaskAuditDetailType>> classSet = reflections.getSubTypesOf(IProcessTaskAuditDetailType.class);
 					for (Class<? extends IProcessTaskAuditDetailType> c : classSet) {
 						try {
-							for(IProcessTaskAuditDetailType type : c.getEnumConstants()) {
-								set.add(type);
-							}
-						}catch(Exception e) {
-							
-						}		
-					}
+                            set.addAll(Arrays.asList(c.getEnumConstants()));
+                        } catch (Exception ignored) {
+
+                        }
+                    }
 					isUninitialized = false;
 				}
 			}
--- src/main/java/neatlogic/framework/process/constvalue/ProcessStepHandlerType.java
+++ src/main/java/neatlogic/framework/process/constvalue/ProcessStepHandlerType.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package neatlogic.framework.process.constvalue;
 
 import neatlogic.framework.process.stephandler.core.IProcessStepHandlerType;
@@ -11,14 +27,13 @@ public enum ProcessStepHandlerType implements IProcessStepHandlerType {
     DISTRIBUTARY("distributary", "converge", new I18n("分流")),
     OCTOPUS("octopus", "process", new I18n("自动化")),
     AUTOMATIC("automatic", "process", new I18n("自动处理")),
-    TIMER("timer", "process", new I18n("定时节点"))
-    ;
+    TIMER("timer", "process", new I18n("定时节点"));
 
-    private String handler;
-    private I18n name;
-    private String type;
+    private final String handler;
+    private final I18n name;
+    private final String type;
 
-    private ProcessStepHandlerType(String _handler, String _type, I18n _name) {
+    ProcessStepHandlerType(String _handler, String _type, I18n _name) {
         this.handler = _handler;
         this.type = _type;
         this.name = _name;
