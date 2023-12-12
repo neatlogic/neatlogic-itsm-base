87d5027a1412e8841557b9a4e7f32d74d041d4d2
linbangquan <1437892690@qq.com>
2023-12-12 11:37:01 +0800

    [功能] 后端-IT服务跨环境导出再导入
    关联 #[1031708374171648]后端-IT服务跨环境导出再导入 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1031708374171648

--- src/main/java/neatlogic/framework/process/exception/score/ScoreTemplateNotFoundException.java
+++ src/main/java/neatlogic/framework/process/exception/score/ScoreTemplateNotFoundException.java
@@ -7,10 +7,10 @@ public class ScoreTemplateNotFoundException extends ProcessTaskRuntimeException 
 	private static final long serialVersionUID = -2067466892281101344L;
 
 	public ScoreTemplateNotFoundException(Long id) {
-		super("评分模版 :{0}不存在", id);
+		super("nfpes.scoretemplatenotfoundexception.scoretemplatenotfoundexception", id);
 	}
 
 	public ScoreTemplateNotFoundException(String name) {
-		super("评分模版 :{0}不存在", name);
+		super("nfpes.scoretemplatenotfoundexception.scoretemplatenotfoundexception", name);
 	}
 }
