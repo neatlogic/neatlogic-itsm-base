356b275267f67c9d2232eb3fa63107913f2d135f
chenqw <zenkiray@live.com>
2024-03-18 14:35:48 +0800

    更新LICENSE为AGPL3.0
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/framework/process/stephandler/core/IProcessStepInternalHandler.java
+++ src/main/java/neatlogic/framework/process/stephandler/core/IProcessStepInternalHandler.java
@@ -18,19 +18,7 @@ import java.util.Map;
  * @Description: 处理流程组件内部业务逻辑
  * @Author: chenqiwei
  * @Date: 2021/1/20 15:55
-Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
  **/
 public interface IProcessStepInternalHandler {
 
