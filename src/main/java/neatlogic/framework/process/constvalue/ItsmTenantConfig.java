5a269795cf2a451ba44045f601b40d4570597325
1437892690@qq.com <1437892690@qq.com>
2025-01-02 15:51:03 +0800

    [修复] 工单详情页全局默认tab固定&顺序
    关联 #[1325213151625216]工单详情页全局默认tab固定&顺序 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1325213151625216

--- src/main/java/neatlogic/framework/process/constvalue/ItsmTenantConfig.java
+++ src/main/java/neatlogic/framework/process/constvalue/ItsmTenantConfig.java
@@ -27,6 +27,7 @@ public enum ItsmTenantConfig implements ITenantConfig {
     WORKCENTER_AUTO_REFRESH("workcenter.auto.refresh", "1", "nfpc.itsmtenantconfig.workcenterrefresh"),
     WORKCENTER_CUSTOM_LIMIT("workcenter.custom.limit", "5", "nfpc.itsmtenantconfig.workcentercustomlimit"),
     WORKCENTER_PROCESSTASK_NEWPAGE("workcenter.processtask.newpage", "0", "nfpc.itsmtenantconfig.workcenterprocesstasknewpage"),
+    PROCESSTASK_TAB_LAYOUT("processtask.tab.layout", "{}", "nfpc.itsmtenantconfig.processtasktablayout"),
     ;
 
     String key;
