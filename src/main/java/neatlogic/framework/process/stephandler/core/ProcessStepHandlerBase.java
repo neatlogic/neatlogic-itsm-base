5d57605a9cb78b16a0930af5e1f30b78baf1285e
1437892690@qq.com <1437892690@qq.com>
2025-02-15 17:14:37 +0800

    [修复] 工单上报后提示工单未提交
    关联 #[1357283789864960]工单上报后提示工单未提交 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1357283789864960

--- src/main/java/neatlogic/framework/process/stephandler/core/ProcessStepHandlerBase.java
+++ src/main/java/neatlogic/framework/process/stephandler/core/ProcessStepHandlerBase.java
@@ -121,7 +121,8 @@ public abstract class ProcessStepHandlerBase implements IProcessStepHandler {
     private int updateProcessTaskStatus(Long processTaskId) {
         IProcessTaskCrossoverMapper processTaskCrossoverMapper = CrossoverServiceFactory.getApi(IProcessTaskCrossoverMapper.class);
         List<ProcessTaskStepVo> processTaskStepList = processTaskCrossoverMapper.getProcessTaskStepBaseInfoByProcessTaskId(processTaskId);
-        int runningCount = 0, succeedCount = 0, failedCount = 0, abortedCount = 0, draftCount = 0, hangCount = 0, endSucceedCount = 0;
+        // 分组统计不同步骤状态的数量
+        int runningCount = 0, succeedCount = 0, failedCount = 0, abortedCount = 0, draftCount = 0, hangCount = 0, endSucceedCount = 0, startSucceedCount = 0;
         for (ProcessTaskStepVo processTaskStepVo : processTaskStepList) {
             if (ProcessTaskStepStatus.DRAFT.getValue().equals(processTaskStepVo.getStatus())
                     && processTaskStepVo.getIsActive().equals(1)) {
@@ -145,7 +146,7 @@ public abstract class ProcessStepHandlerBase implements IProcessStepHandler {
                 }
             }
         }
-
+        // 根据步骤状态计算出工单状态
         boolean needCalculateTimeCost = false;
         ProcessTaskVo processTaskVo = new ProcessTaskVo();
         processTaskVo.setId(processTaskId);
@@ -2332,7 +2333,10 @@ public abstract class ProcessStepHandlerBase implements IProcessStepHandler {
             currentProcessTaskStepVo.setStatus(ProcessTaskStepStatus.SUCCEED.getValue());
             currentProcessTaskStepVo.setUpdateEndTime(1);
             updateProcessTaskStepStatus(currentProcessTaskStepVo);
-
+            ProcessTaskVo processTaskVo = new ProcessTaskVo();
+            processTaskVo.setId(currentProcessTaskStepVo.getId());
+            processTaskVo.setStatus(ProcessTaskStatus.RUNNING.getValue());
+            processTaskCrossoverMapper.updateProcessTaskStatus(processTaskVo);
             /* 流转到下一步 **/
             List<ProcessTaskStepThread> processTaskStepThreadList = new ArrayList<>();
             Set<Long> nextStepIdSet = getNext(currentProcessTaskStepVo);
